e66c48631a63f0486f478ecfae0a445fbc9c51f1
Parth Rawat <98413110+Rawat10parth@users.noreply.github.com>
2025-05-06 16:48:11 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // Base URL for API calls
-const API_URL = "unisale-backend-ex5dut2hw-rawat10parths-projects.vercel.app";
+const API_URL = "https://unisale-backend.vercel.app";
 
 export default API_URL;
